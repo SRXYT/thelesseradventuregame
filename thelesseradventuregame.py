c3490f8d5985f73f9fcde8287761b6244b2fdd7f
SRXYT <liampaulapegus@gmail.com>
2025-01-19 20:51:18 +0000

    updates classes made it better

--- thelesseradventuregame.py
+++ thelesseradventuregame.py
@@ -2,9 +2,6 @@ import dearpygui.dearpygui as dpg
 import random
 import time as t
 
-# class Spider:
-#     def __init__(self):
-
 class Player:
     def __init__(self):
        self.player_hp = 100
@@ -25,11 +22,33 @@ class Player:
         damage = random.randint(10, 50)
         if is_crit < 25:
             game.input_to_output("\nCRIT DAMAGE!")
-            game.input_to_output(f"You deal {damage*2} to {enemy}")
+            game.input_to_output(f"You deal {damage*2} to {enemy.name}")
             enemy.lose_hp(damage*2)
         else:
-            game.input_to_output(f"You deal {damage} to {enemy}")
+            game.input_to_output(f"You deal {damage} to {enemy.name}")
             enemy.lose_hp(damage)
+
+class Spider:
+    def __init__(self, name):
+        self.spider_hp = 50
+        self.name = name
+    
+    def lose_hp(self, hp):
+        self.spider_hp -= hp
+    
+    def attack(self, player):
+        is_crit = random.randint(1,200)
+        damage = random.randint(1, 10)
+        if self.spider_hp >= 0:
+            if is_crit < 25:
+                game.input_to_output("\nCRIT DAMAGE!")
+                game.input_to_output(f"Spider deals {damage*2} to {player.get_player_name()}")
+                player.lose_hp(damage*2)
+            else:
+                game.input_to_output(f"Spider deals {damage} to {player.get_player_name()}")
+                player.lose_hp(damage)
+        else:
+            game.input_to_output(f"{self.name} has been defeated!")
 
 
 class Game:
@@ -62,8 +81,29 @@ class Game:
         self.input_received = False
         while not self.input_received and dpg.is_dearpygui_running():
             dpg.render_dearpygui_frame()
+    
+    def battle_menu(self, player, enemy):
+        self.input_to_output("\nWhat will you do?")
+        self.input_to_output("1. Attack")
+        self.input_to_output("2. Use HP Potion")
+        self.input_to_output("3. Check how much HP you have\n")
+        self.wait_for_input()
+        if self.user_input == "1":
+            player.attack(enemy)
+            enemy.attack(player)
+        elif self.user_input == "2":
+            if player.hp_potions > 0:
+                player.hp_potions -= 1
+                player.player_hp += 50
+                self.input_to_output("\nYou used a HP potion and gained 50 HP!")
+                self.input_to_output(f"You have {player.hp_potions} HP potions left")
+                self.input_to_output(f"You now have {player.player_hp} HP\n")
+            else:
+                self.input_to_output("\nYou don't have any HP potions left!\n")
+        elif self.user_input == "3":
+            self.input_to_output(f"\nYou have {player.player_hp} HP left\n")
 
-    def main_game(self):
+    def main_game(self, player):
         self.input_to_output("You are standing in front of a cave. You can see the dragon's lair inside the cave.")
         self.input_to_output("What would you like to do? (enter the cave/leave)")
         self.wait_for_input()
@@ -72,24 +112,37 @@ class Game:
         else:
             self.input_to_output("You consider leaving but decide what's the point in standing in front of a cave if you're not going to enter it, so you enter it anyway.")
         self.input_to_output(f"The cave has {self.num_of_floors} floors to beat before you reach the final boss")
-        self.input_to_output("Are you ready? (Enter anything to continue)")
+        self.input_to_output("Are you ready? (Enter anything to continue)\n")
         self.wait_for_input()
         round = 1
         for i in range(self.easy_floors):
-            self.input_to_output(f"\nFloor {round+1}\n")
+            self.input_to_output(f"\nFloor {round}\n")
+            num_of_spider = random.randint(1, 5)
+            self.input_to_output(f"You see {num_of_spider} spiders ahead!\n")
+            spiders = {}
+            for j in range(num_of_spider):
+                spider_name = "Spider " + str(j + 1)
+                spider = Spider(spider_name)
+                spiders[spider_name] = spider
+            for spider in spiders:
+                self.input_to_output(f"{spider} appears!")
+                while player.player_hp > 0 and spiders[spider].spider_hp > 0:
+                    self.battle_menu(player, spiders[spider])
+                    if player.player_hp <= 0:
+                        self.input_to_output("You have been defeated!")
+                        return
+            round += 1
+        for i in range(self.med_floors):
+            self.input_to_output(f"\nFloor {round}\n")
             num_of_spider = random.randint(1, 5)
             round += 1
-
-
-
-
-    def init_game(self):
+        
+    def init_game(self, player):
         self.input_to_output("WELCOME TO THE GAME")
         self.input_to_output("You are a brave adventurer who has been tasked with saving the kingdom from the evil dragon")
         self.input_to_output("Please enter your character name: ")
         self.wait_for_input()  # Wait for user input
         player_name = self.user_input  # Get the entered name
-        player = Player()
         player.set_player_name(player_name)
         self.input_to_output(f"Welcome to the game {player.get_player_name()}!")
         self.input_to_output("\nYou are about to embark on a dangerous journey to defeat the dragon and save the kingdom.")
@@ -100,7 +153,7 @@ class Game:
             self.input_to_output("Great! Let's get started.")
         else:
             self.input_to_output("That's too bad.")
-        self.main_game()
+        self.main_game(player)
 
     def setup_gui(self):
         dpg.create_context()
@@ -117,18 +170,20 @@ class Game:
                         no_resize=True, no_move=True, no_close=True, no_collapse=True, no_title_bar=True, pos=(10, 10)):
             dpg.add_input_text(default_value=self.console_text, readonly=True,  # Make it non-editable
                                tag="output_box", multiline=True, width=self.output_window_width-20,
-                               height=self.output_window_height-20, pos=(10, 10), callback=None, tracked=True)
+                               height=self.output_window_height-20, pos=(10, 10), callback=None, tracked=True,
+                               track_offset=1.0)
 
         dpg.setup_dearpygui()
         dpg.show_viewport()
         dpg.set_primary_window("primary_window", True)
 
-    def start(self):
+    def start(self, player):
         self.setup_gui()
-        self.init_game()
+        self.init_game(player)
         dpg.start_dearpygui()
         dpg.destroy_context()
 
 # Run the game
+player = Player()
 game = Game()
-game.start()
+game.start(player)
